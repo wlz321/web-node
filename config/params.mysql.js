d4232cb4ffaccb21887f7fc87e39903f77411c9c
wlz321 <2505013465@qq.com>
2020-09-03 01:36:35 +0800

    config mysql

--- config/params.mysql.js
+++ config/params.mysql.js
@@ -1,7 +1,7 @@
 module.exports = {
-    host:'172.17.0.3',
+    host:'127.0.0.1',
     port:'3306',
-    username:'root',
-    password:'mnbvcxz@123',
-    database:'test'
+    username:'chw',
+    password:'chw@Wifi654321',
+    database:'lobby'
 }
